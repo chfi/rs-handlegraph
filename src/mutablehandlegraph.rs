fad41fc54ea599c117a0a03031b9775968dd9cfb
Christian <christian@chfi.se>
2020-06-28 17:49:31 -0500

    split_handle

--- src/mutablehandlegraph.rs
+++ src/mutablehandlegraph.rs
@@ -15,16 +15,19 @@ pub trait MutableHandleGraph: HandleGraph {
         offsets: Vec<usize>,
     ) -> Vec<Handle>;
 
+    fn split_handle(
+        &mut self,
+        handle: Handle,
+        offset: usize,
+    ) -> (Handle, Handle) {
+        let handles = self.divide_handle(handle, vec![offset]);
+        (handles[0], handles[1])
+    }
+
     /*
 
     // this needs some additional functions first, such as reverse complement
     fn apply_orientation(&mut self, handle: &Handle) -> Handle;
-
-    fn split_handle(
-        &mut self,
-        handle: &Handle,
-        offset: usize,
-    ) -> (Handle, Handle);
 
     */
 
