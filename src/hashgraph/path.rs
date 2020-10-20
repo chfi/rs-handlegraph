c1558bd64e9d455c829317f9ab4da9c03bbeb200
Christian <christian@chfi.se>
2020-10-19 22:11:17 -0500

    Implement some step & base pos. related methods on Path

--- src/hashgraph/path.rs
+++ src/hashgraph/path.rs
@@ -1,6 +1,9 @@
 use bstr::BString;
+use std::collections::HashMap;
 
-use crate::handle::Handle;
+use crate::handle::{Handle, NodeId};
+
+use super::Node;
 
 pub type PathId = i64;
 
@@ -51,11 +54,64 @@ impl Path {
         }
     }
 
+    pub fn len(&self) -> usize {
+        self.nodes.len()
+    }
+
+    pub fn bases_len(&self, graph: &HashMap<NodeId, Node>) -> usize {
+        self.nodes
+            .iter()
+            .filter_map(|handle| {
+                graph.get(&handle.id()).map(|n| n.sequence.len())
+            })
+            .sum()
+    }
+
     pub fn lookup_step_handle(&self, step: &PathStep) -> Option<Handle> {
         match step {
             PathStep::Front(_) => None,
             PathStep::End(_) => None,
             PathStep::Step(_, ix) => Some(self.nodes[*ix]),
         }
+    }
+
+    pub fn position_of_step(
+        &self,
+        graph: &HashMap<NodeId, Node>,
+        step: &PathStep,
+    ) -> Option<usize> {
+        if step.path_id() != self.path_id {
+            return None;
+        }
+
+        match step {
+            PathStep::Front(_) => Some(0),
+            PathStep::End(_) => Some(self.bases_len(graph)),
+            &PathStep::Step(_, step_ix) => {
+                let mut bases = 0;
+                for handle in self.nodes[0..step_ix].iter() {
+                    let node = graph.get(&handle.id())?;
+                    bases += node.sequence.len();
+                }
+                Some(bases)
+            }
+        }
+    }
+
+    pub fn step_at_position(&self, pos: usize) -> PathStep {
+        if pos == 0 {
+            return PathStep::Front(*self.path_id);
+        }
+
+        let mut bases = 0;
+        for (ix, handle) in self.nodes.iter().enumerate() {
+            let node = graph.get(&handle.id()).unwrap();
+            bases += node.sequence.len();
+            if pos < bases {
+                return PathStep::Step(*self.path_id, ix);
+            }
+        }
+
+        return PathStep::End(*self.path_id);
     }
 }
