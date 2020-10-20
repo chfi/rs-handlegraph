b57c4ecd4d5d939d6a9c74de40f36d87dc05470e
Christian <christian@chfi.se>
2020-10-20 16:38:18 -0500

    Implement PagedIntVector

--- src/packed.rs
+++ src/packed.rs
@@ -125,6 +125,160 @@ impl PartialEq for PackedIntVec {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct PagedIntVec {
+    page_size: usize,
+    num_entries: usize,
+    anchors: PackedIntVec,
+    pages: Vec<PackedIntVec>,
+}
+
+impl PagedIntVec {
+    pub fn new(page_size: usize) -> Self {
+        let num_entries = 0;
+        let pages = Vec::new();
+        let anchors = Default::default();
+        PagedIntVec {
+            page_size,
+            num_entries,
+            anchors,
+            pages,
+        }
+    }
+
+    #[inline]
+    pub fn page_width(&self) -> usize {
+        self.page_size
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.num_entries
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.pages.clear();
+        self.anchors.clear();
+        self.num_entries = 0;
+    }
+
+    #[inline]
+    pub fn resize(&mut self, new_size: usize) {
+        if new_size < self.num_entries {
+            let num_pages = if new_size == 0 {
+                0
+            } else {
+                (new_size - 1) / self.page_size + 1
+            };
+
+            self.anchors.resize(num_pages);
+            self.pages.resize_with(num_pages, Default::default);
+        } else if new_size > self.num_entries {
+            self.reserve(new_size);
+        }
+
+        self.num_entries = new_size;
+    }
+
+    #[inline]
+    pub fn reserve(&mut self, new_size: usize) {
+        if new_size > self.pages.len() * self.page_size {
+            let num_pages = (new_size - 1) / self.page_size + 1;
+
+            self.anchors.reserve(num_pages);
+            self.pages.reserve(num_pages - self.pages.len());
+
+            self.anchors.resize(num_pages);
+            while num_pages > self.pages.len() {
+                let mut new_page = PackedIntVec::new();
+                new_page.resize(self.page_size);
+                self.pages.push(new_page);
+            }
+        }
+    }
+
+    #[inline]
+    pub fn set(&mut self, index: usize, value: u64) {
+        assert!(index < self.num_entries);
+
+        let page_ix = index / self.page_size;
+        let mut anchor = self.anchors.get(page_ix);
+
+        if anchor == 0 {
+            self.anchors.set(page_ix, value);
+            anchor = value;
+        }
+
+        self.pages[page_ix]
+            .set(index % self.page_size, Self::to_diff(value, anchor));
+    }
+
+    #[inline]
+    pub fn get(&self, index: usize) -> u64 {
+        assert!(index < self.num_entries);
+        let page_ix = index / self.page_size;
+        Self::from_diff(
+            self.pages[page_ix].get(index % self.page_size),
+            self.anchors.get(page_ix),
+        )
+    }
+
+    #[inline]
+    pub fn append(&mut self, value: u64) {
+        if self.num_entries == self.pages.len() * self.page_size {
+            let mut new_page = PackedIntVec::new();
+            new_page.resize(self.page_size);
+            self.anchors.append(0);
+            self.pages.push(new_page);
+        }
+
+        self.num_entries += 1;
+        self.set(self.num_entries - 1, value);
+    }
+
+    #[inline]
+    pub fn pop(&mut self) {
+        self.num_entries -= 1;
+
+        while self.num_entries + self.page_size
+            <= self.pages.len() * self.page_size
+        {
+            self.pages.pop();
+            self.pages.shrink_to_fit();
+            self.anchors.pop();
+        }
+    }
+
+    #[inline]
+    const fn to_diff(value: u64, anchor: u64) -> u64 {
+        if value == 0 {
+            0
+        } else if value >= anchor {
+            let raw_diff = value - anchor;
+            raw_diff + raw_diff / 4 + 1
+        } else {
+            5 * (anchor - value)
+        }
+    }
+
+    #[inline]
+    const fn from_diff(diff: u64, anchor: u64) -> u64 {
+        if diff == 0 {
+            0
+        } else if diff % 5 == 0 {
+            anchor - diff / 5
+        } else {
+            anchor + diff - diff / 5 - 1
+        }
+    }
+}
+
 use quickcheck::{Arbitrary, Gen};
 
 impl Arbitrary for PackedIntVec {
