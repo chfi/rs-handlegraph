b5722d850fc218be324699e2085b1c4bdc235b1e
Christian <christian@chfi.se>
2020-12-20 17:38:47 -0600

    Set up initial algorithm state & data

--- src/consensus.rs
+++ src/consensus.rs
@@ -1,12 +1,16 @@
 use crate::{
     handle::{Edge, Handle, NodeId},
-    handlegraph::HandleGraphRef,
+    handlegraph::*,
     mutablehandlegraph::*,
     pathhandlegraph::*,
 };
 
 use crate::packedgraph::paths::StepPtr;
 use crate::packedgraph::*;
+
+use fnv::{FnvHashMap, FnvHashSet};
+
+use rayon::prelude::*;
 
 #[derive(Debug, Clone)]
 pub struct LinkPath {
@@ -80,4 +84,53 @@ pub struct LinkRange {
     start: NodeId,
     end: NodeId,
     path: PathId,
+}
+
+pub fn create_consensus_graph(
+    smoothed: &PackedGraph,
+    consensus_path_names: &[Vec<u8>],
+    consensus_jump_max: usize,
+    base: Vec<u8>,
+) -> PackedGraph {
+    let mut res_graph = PackedGraph::default();
+
+    let consensus_paths: Vec<PathId> = consensus_path_names
+        .iter()
+        .filter_map(|path_name| smoothed.get_path_id(path_name))
+        .collect();
+
+    let consensus_path_ptrs: FnvHashMap<PathId, &[u8]> = consensus_path_names
+        .iter()
+        .filter_map(|path_name| {
+            let path_id = smoothed.get_path_id(path_name)?;
+            Some((path_id, path_name.as_slice()))
+        })
+        .collect();
+
+    let is_consensus: Vec<bool> = smoothed
+        .path_ids()
+        .map(|path_id| consensus_paths.contains(&path_id))
+        .collect();
+
+    let mut handle_is_consensus: Vec<bool> = vec![false; smoothed.node_count()];
+    let mut handle_consensus_path_ids: Vec<PathId> =
+        vec![PathId(0); smoothed.node_count()];
+
+    for &path_id in consensus_paths.iter() {
+        if let Some(path_ref) = smoothed.get_path_ref(path_id) {
+            for step in path_ref.steps() {
+                let node_id = step.handle().id();
+                let index = usize::from(node_id) - 1;
+                handle_is_consensus[index] = true;
+                handle_consensus_path_ids[index] = path_id;
+            }
+        }
+    }
+
+    let non_consensus_paths: Vec<PathId> = smoothed
+        .path_ids()
+        .filter(|path_id| is_consensus[path_id.0 as usize])
+        .collect();
+
+    res_graph
 }
