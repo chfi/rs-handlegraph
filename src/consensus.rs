f45c3331e755ccb8fff982ba34763ce09cd10379
Christian <christian@chfi.se>
2020-12-20 16:12:02 -0600

    Impl PartialEq & Ord for LinkPath matching C++ version's < def

--- src/consensus.rs
+++ src/consensus.rs
@@ -8,7 +8,7 @@ use crate::{
 use crate::packedgraph::paths::StepPtr;
 use crate::packedgraph::*;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone)]
 pub struct LinkPath {
     pub from_cons_name: Vec<u8>,
     pub to_cons_name: Vec<u8>,
@@ -22,6 +22,57 @@ pub struct LinkPath {
     is_reverse: bool,
     jump_len: usize,
     rank: u64,
+}
+
+impl PartialEq for LinkPath {
+    fn eq(&self, other: &Self) -> bool {
+        let self_from = &self.from_cons_path;
+        let self_to = &self.to_cons_path;
+        let other_from = &other.from_cons_path;
+        let other_to = &other.to_cons_path;
+
+        (self_from == other_from)
+            && (self_to == other_to)
+            && (self.length == other.length)
+            && (self.hash == other.hash)
+    }
+}
+
+impl PartialOrd for LinkPath {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        use std::cmp::Ordering;
+
+        let self_from = &self.from_cons_path;
+        let self_to = &self.to_cons_path;
+        let other_from = &other.from_cons_path;
+        let other_to = &other.to_cons_path;
+
+        if self_from < other_from {
+            return Some(Ordering::Less);
+        }
+
+        if self_from == other_from {
+            if self_to < other_to {
+                return Some(Ordering::Less);
+            }
+
+            if self_to == other_to {
+                if self.length < other.length {
+                    return Some(Ordering::Less);
+                }
+
+                if self.length == other.length && self.hash < other.hash {
+                    return Some(Ordering::Less);
+                }
+            }
+        }
+
+        if self == other {
+            return Some(Ordering::Equal);
+        } else {
+            return Some(Ordering::Greater);
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
