1167a79882614ba5623252653f390174d5caf486
Christian <christian@chfi.se>
2021-01-06 13:22:35 -0600

    deltas for tracking graph properties over ops

--- src/packedgraph/quickcheck.rs
+++ src/packedgraph/quickcheck.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 
 #[allow(unused_imports)]
 use crate::{
@@ -14,6 +15,21 @@ use crate::{
         PathSteps,
     },
 };
+
+use super::{
+    edges::{EdgeListIx, EdgeLists},
+    index::{list, OneBasedIndex, RecordIndex},
+    iter::EdgeListHandleIter,
+    nodes::IndexMapIter,
+    occurrences::OccurrencesIter,
+    paths::packedpath::StepPtr,
+    sequence::DecodeIter,
+    PackedGraph,
+};
+
+use quickcheck::{Arbitrary, Gen, QuickCheck};
+
+use fnv::{FnvHashMap, FnvHashSet};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum GraphOp {
@@ -58,4 +74,52 @@ pub enum MutPathOp {
 pub enum GraphWideOp {
     Defragment,
     ApplyOrdering { order: Vec<Handle> },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct NodePropertiesDelta {
+    pub node_count: isize,
+    pub total_len: isize,
+    pub max_id: NodeId,
+    pub new_handles: Vec<(Handle, Vec<u8>)>,
+    pub removed_handles: Vec<Handle>,
+}
+
+pub struct LocalEdgeDelta {
+    pub handle: Handle,
+    pub new_left: Vec<Edge>,
+    pub new_right: Vec<Edge>,
+    pub removed_left: Vec<Edge>,
+    pub removed_right: Vec<Edge>,
+    pub left_degree: isize,
+    pub right_degree: isize,
+}
+
+pub struct EdgePropertiesDelta {
+    pub edge_count: isize,
+    pub new_edges: Vec<Edge>,
+    pub removed_edges: Vec<Edge>,
+    pub edge_deltas: Vec<LocalEdgeDelta>,
+}
+
+pub struct PathPropertiesDelta {
+    pub path_count: isize,
+    pub total_steps: isize,
+    pub new_paths: Vec<(PathId, Vec<u8>)>,
+    pub removed_paths: Vec<(PathId, Vec<u8>)>,
+}
+
+pub struct LocalStep {
+    pub prev: (StepPtr, Handle),
+    pub this: (StepPtr, Handle),
+    pub next: (StepPtr, Handle),
+}
+
+pub struct SinglePathPropertiesDelta {
+    pub step_count: isize,
+    pub seq_len: isize,
+    pub new_steps: Vec<LocalStep>,
+    pub removed_steps: Vec<StepPtr>,
+    pub new_head: StepPtr,
+    pub new_tail: StepPtr,
 }
