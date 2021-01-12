bba215fe50b1d206d3cdb93d666384924616196f
Christian Fischer <christian@chfi.se>
2021-01-11 19:48:41 -0600

    generating edge add/remove ops

--- src/packedgraph/quickcheck.rs
+++ src/packedgraph/quickcheck.rs
@@ -42,6 +42,90 @@ pub use delta::{
 use ops::{CreateOp, GraphOp, GraphWideOp, MutHandleOp, MutPathOp, RemoveOp};
 use traits::{DeriveDelta, GraphApply, GraphDelta};
 
+use rand::prelude::*;
+
+fn gen_edge_ops(edges: &[Edge], mut del_r: f64, shuffle: bool) -> Vec<GraphOp> {
+    // `del_r` signifies to what extent edges will be removed and re-added;
+    // 0.0 -> just add all edges
+    // 1.0 -> all edges will be removed and re-added at least once
+    del_r = del_r.max(0.0).min(1.0);
+
+    // if `shuffle` is true, the edge create ops will be in random
+    // order, if false, the same order as the provided `edges` slice
+
+    let create_op = |edge: Edge| -> GraphOp {
+        GraphOp::Create {
+            op: CreateOp::Edge { edge },
+        }
+    };
+
+    let remove_op = |edge: Edge| -> GraphOp {
+        GraphOp::Remove {
+            op: RemoveOp::Edge { edge },
+        }
+    };
+
+    if del_r < f64::EPSILON {
+        return edges.iter().map(|&edge| create_op(edge)).collect();
+    }
+
+    let mut rng = rand::thread_rng();
+
+    // edges that will be removed and added back in
+    let mut remove_add: FnvHashSet<Edge> = FnvHashSet::default();
+
+    for &edge in edges.iter() {
+        let v: f64 = rng.gen();
+        if v <= del_r {
+            remove_add.insert(edge);
+        }
+    }
+
+    let edges: Vec<Edge> = if shuffle {
+        edges
+            .choose_multiple(&mut rng, edges.len())
+            .copied()
+            .collect()
+    } else {
+        edges.to_vec()
+    };
+
+    let mut remove: Vec<(usize, Edge)> = Vec::new();
+
+    let mut ops = Vec::with_capacity(edges.len());
+
+    for edge in edges {
+        if remove_add.contains(&edge) {
+            remove.push((ops.len(), edge));
+        }
+        ops.push(create_op(edge));
+    }
+
+    let mut readd: Vec<(usize, Edge)> = Vec::new();
+
+    let mut count = 0usize;
+
+    // `ix` is the index of the edge's create op, so we know to add the remove op somewhere after
+    for &(ix, edge) in remove.iter() {
+        let ix = ix + count;
+        let rem_ix = rng.gen_range(ix + 1, ops.len() + 1);
+        ops.insert(rem_ix, remove_op(edge));
+        readd.push((rem_ix, edge));
+        count += 1;
+    }
+
+    count = 0;
+
+    for &(ix, edge) in readd.iter() {
+        let ix = ix + count;
+        let add_ix = rng.gen_range(ix + 1, ops.len() + 1);
+        ops.insert(add_ix, create_op(edge));
+        count += 1;
+    }
+
+    ops
+}
+
 #[test]
 fn adding_nodes_prop() {
     let mut graph_1 = crate::packedgraph::tests::test_graph_no_paths();
